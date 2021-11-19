da70db40f87a48cc7e9f89bd096d9f687b812360
Lucio Paiva <luciopaiva@gmail.com>
2021-11-19 22:55:31 +0000

    Simplify HighLatencyService

--- src/main/java/dao/HighLatencyService.java
+++ src/main/java/dao/HighLatencyService.java
@@ -1,26 +1,20 @@
 package dao;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class HighLatencyService {
 
-    private final Map<Integer, String> stringById = new HashMap<>();
     private final AtomicInteger accessCount = new AtomicInteger();
     private final int latencyInMillis;
 
     public HighLatencyService(int latencyInMillis) {
         this.latencyInMillis = latencyInMillis;
-
-        stringById.put(1, "foo");
-        stringById.put(2, "bar");
     }
 
     public String fetchById(int id) {
         sleep(latencyInMillis);
         accessCount.incrementAndGet();
-        return stringById.get(id);
+        return String.valueOf(id);
     }
 
     public int getAccessCount() {
